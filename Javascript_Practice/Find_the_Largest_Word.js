5b926da424d0ed9373f7260abbef6ffb938a1121
chirag jariya <143921296+chiragpjariya@users.noreply.github.com>
2024-08-02 09:52:39 +0530

    Created Protottpe of largset word finder

---
+++ Javascript_Practice/Find_the_Largest_Word.js
@@ -0,0 +1,29 @@
+// Create Largest Word Finder prototype
+let str = "ajcncnjdn sncjncj   naskjkcnjcjnfcf ancjjcnjec ncn";
+
+
+String.prototype.LargestWord = function () {
+    let strToArray = this.split(" ")
+    let strArrayEachElementLengthStore = [];
+    let maximumLengthFromElement;
+    let largestWord;
+
+    strToArray.forEach((word) => {
+        strArrayEachElementLengthStore.push(word.length)
+    })
+
+    maximumLengthFromElement = Math.max(...strArrayEachElementLengthStore)
+
+    strToArray.forEach((items) => {
+        if (items.length === maximumLengthFromElement) {
+            largestWord = items
+        }
+    })
+
+    return largestWord
+
+
+}
+
+
+console.log(str.LargestWord())
