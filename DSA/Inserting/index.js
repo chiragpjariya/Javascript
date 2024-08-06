9bea78a93385874a511e9a27d536124dec843124
chirag jariya <143921296+chiragpjariya@users.noreply.github.com>
2024-08-06 17:01:32 +0530

    New update

--- DSA/Inserting/index.js
+++ DSA/Inserting/index.js
@@ -11,6 +11,7 @@ let newEl = 10.10;
 
 console.log(Array);
 
+Without InBuilde Fuction .
 for (let i = Array.length - 1; i >= 0; i--) {
     if (i >= pos) {
         Array[i + 1] = Array[i]
@@ -22,4 +23,9 @@ for (let i = Array.length - 1; i >= 0; i--) {
 }
 
 
-console.log(Array)
+// console.log(Array)
+// In Builde fucntion
+// Array.splice(position,delete,newELement)
+
+// Array.splice(2,0,2000)
+// console.log(Array);
