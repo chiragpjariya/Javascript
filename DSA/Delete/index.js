93c0e15010a36c5789964620bc108a04cf4283c6
chirag jariya <143921296+chiragpjariya@users.noreply.github.com>
2024-08-06 17:21:23 +0530

    Optimization

--- DSA/Delete/index.js
+++ DSA/Delete/index.js
@@ -6,15 +6,23 @@ let arr = [10, 20, 30, 40, 50, 60]
 let pos = 2;
 
 // Without in build fucntion
-for (let i = 0; i <= arr.length - 1; i++) {
-    if (i >= pos) {
-        arr[i] = arr[i + 1]
-        if (i == arr.length - 1) {
-            arr.length = arr.length - 1
-        }
+// for (let i = 0; i <= arr.length - 1; i++) {
+//     if (i >= pos) {
+//         arr[i] = arr[i + 1]
+//         if (i == arr.length - 1) {
+//             arr.length = arr.length - 1
+//         }
+//     }
+// }
+
+
+
+for (let i = pos; i <= arr.length - 1; i++) {
+    arr[i] = arr[i + 1]
+    if (i == arr.length - 1) {
+        arr.length = arr.length - 1
     }
 }
-
 
 // In build fucntion
 // arr.splice(3,1)
