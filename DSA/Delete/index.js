2adce54d9a19e82febc29da6f73421f3b56a1066
chirag jariya <143921296+chiragpjariya@users.noreply.github.com>
2024-08-06 17:16:40 +0530

    Delete element

---
+++ DSA/Delete/index.js
@@ -0,0 +1,23 @@
+// Delete elements from Array
+// loop starting pointe are start and stop in ending
+
+
+let arr = [10, 20, 30, 40, 50, 60]
+let pos = 2;
+
+// Without in build fucntion
+for (let i = 0; i <= arr.length - 1; i++) {
+    if (i >= pos) {
+        arr[i] = arr[i + 1]
+        if (i == arr.length - 1) {
+            arr.length = arr.length - 1
+        }
+    }
+
+}
+
+
+// In build fucntion
+// arr.splice(3,1)
+
+console.log(arr);
