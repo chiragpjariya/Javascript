d331ced2f19c72c1b13f0978ba575320b74fc403
chirag jariya <143921296+chiragpjariya@users.noreply.github.com>
2024-08-06 16:00:02 +0530

    .

--- DSA/Inserting/index.js
+++ DSA/Inserting/index.js
@@ -5,21 +5,21 @@
 // :right To Left
 
 
-let Array = [10,20,30,40,50,60,70,80,90]
+let Array = [10, 20, 30, 40, 50, 60, 70, 80, 90]
 let pos = 2;
 let newEl = 10.10;
 
-    console.log(Array);
-    
-    for(let i = Array.length-1;i>=0;i--){
-        if(i>=pos){
-         Array[i+1]=Array[i]
-         if(i==pos){
-            Array[i]=newEl
-         }
-        } 
-       
+console.log(Array);
+
+for (let i = Array.length - 1; i >= 0; i--) {
+    if (i >= pos) {
+        Array[i + 1] = Array[i]
+        if (i == pos) {
+            Array[i] = newEl
+        }
     }
+
+}
 
 
 console.log(Array)
